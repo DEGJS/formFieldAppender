ca08c03e1226b87840c47e51e69a6f1b726183d1
Aaron Ladage <aladage@degdigital.com>
2017-03-14 11:19:43 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,154 @@
 # formFieldAppender
-A module for adding and saving an infinite number of form fields.
+formFieldAppender is a Javascript module that allows you to add and remove an infinite number of similar form fields to a form.
+
+## Install
+formFieldAppender is an ES6 module. Consequently, you'll need an ES6 transpiler ([Babel](https://babeljs.io) is a nice one) and a module loader ([SystemJS](https://github.com/systemjs/systemjs) will do the job) as part of your Javascript workflow.
+
+If you're already using the [JSPM package manager](http://jspm.io) for your project, you can install formFieldAppender with the following command:
+
+```
+$ jspm install github:DEGJS/formFieldAppender
+```
+
+## Usage
+Import formFieldAppender and create a new instance of it.
+```js
+import formFieldAppender from 'DEGJS/formFieldAppender';
+
+let element = document.querySelector('div');
+
+let instance = formFieldAppender(element);
+```
+
+By default, formFieldAppender will search the supplied `element` for an item that matches the `itemClass` parameter to use as its blueprint.
+
+However, you can optionally provide either an HTML element or string representation of an HTML element as the item blueprint.
+
+#### As an HTML element:
+```js
+import formFieldAppender from 'DEGJS/formFieldAppender';
+
+let element = document.querySelector('div');
+let itemBlueprint = document.createElement('div');
+
+itemBlueprint.classList.add('phone-field__wrapper');
+itemBlueprint.insertAdjacentHTML('afterbegin', `
+    <label for="phone-field">Field</label>
+    <input class="phone-field" name="phone-field" id="phone-field" type="tel">
+`);
+
+let instance = formFieldAppender(element, {
+    blueprint: itemBlueprint
+});
+```
+
+#### As a string representation of an HTML element:
+```js
+import formFieldAppender from 'DEGJS/formFieldAppender';
+
+let element = document.querySelector('div');
+let itemBlueprint = `
+    <div class="phone-field__wrapper">
+        <label for="phone-field">Field</label>
+        <input class="phone-field" name="phone-field" id="phone-field" type="tel">
+    </div>
+`);
+
+let instance = formFieldAppender(element, {
+    blueprint: itemBlueprint
+});
+```
+
+## Parameters
+
+### blueprint
+Type: `HTMLElement` or `string` 
+An HTML element or string representation of an HTML element that will be used as the blueprint for the repeating field.
+Default: null
+
+### itemClass
+Type: `string` 
+The class applied to each appended item.
+Default: `js-ffa-item`
+
+### addTriggerSelector
+Type: `string` 
+The selector for each item's add trigger.
+Default: `.js-ffa-add-trigger`
+
+### removeTriggerSelector
+Type: `string` 
+The selector for each item's remove trigger.
+Default: `.js-ffa-remove-trigger`
+
+### idPatternAttr
+Type: `string` 
+The attribute that contains the pattern that will be used to create each appended item's ID. A token of `[[index]]` in the pattern will be dynamically replaced by the item's current index.
+Default: `data-ffa-id-pattern`
+
+### namePatternAttr
+Type: `string` 
+The attribute that contains the pattern that will be used to create each appended item's `name` attribute. A token of `[[index]]` in the pattern will be dynamically replaced by the item's current index.
+Default: `data-ffa-name-pattern`
+
+### forPatternAttr
+Type: `string` 
+The attribute that contains the pattern that will be used to create each appended item's `for` attribute. A token of `[[index]]` in the pattern will be dynamically replaced by the item's current index.
+Default: `data-ffa-for-pattern`
+
+### disabledClass
+Type: `string` 
+The class applied to an item when it's hidden/disabled. This class can be used to visibly hide disabled items with CSS.
+Default: `is-disabled`
+
+### initialReindex
+Type: `number` 
+The first index value applied to added items.
+Default: `1`
+
+### firstItemIsRemovable
+Type: `boolean` 
+Determines whether the first item can be removed.
+Default: `true`
+
+### onlyLastItemIsRemovable
+Type: `boolean` 
+Determines whether a "remove" trigger is added to all items, or only the last item.
+Default: `false`
+
+### onItemAddCallback
+Type: `function` or `null`
+An optional callback that's fired whenever an item is added.
+Default: `null`
+
+### onItemRemoveCallback
+Type: `function` or `null`
+An optional callback that's fired whenever an item is removed.
+Default: `null`
+
+### resetNewItemFormVals
+Type: `boolean`
+Determines whether the fields in added items contain the values of the original item/template. 
+Default: `true`
+
+
+## Methods
+
+### .destroy()
+Parameters: none  
+Destroys the formFieldAppender instance.
+
+### .getItems()
+Parameters: none  
+Returns an array of the items currently added to the formFieldAppender instance.
+
+
+
+## Browser Support
+
+formFieldAppender depends on the following browser APIs:
++ Object.assign: [Documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign) | [Polyfill](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill)
++ Array.from: [Documentation](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from) | [Polyfill](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from#Polyfill)
++ Element.closest: [Documentation](https://developer.mozilla.org/en-US/docs/Web/API/Element/closest) | [Polyfill](https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill)
+
+To support legacy browsers, you'll need to include polyfills for the above APIs.
---
+++ src/formFieldAppender.js
@@ -0,0 +1,213 @@
+import { emptyElements, isElement } from "DEGJS/domUtils";
+
+let formFieldAppender = function(wrapperEl, options) {
+
+	const errors = {
+			prefix: 'formFieldAppender: ',
+			noWrapperEl: 'No wrapper element found.',
+			noItemElsFoundInMarkup: 'No item elements found in markup.',
+			noItemElsFoundInBlueprint: 'No item elements found in item blueprint.'
+		},
+		defaultPatterns = {
+			id: 'ffa-id-[[index]]',
+			name: 'ffa-name-[[index]]',
+			for: 'ffa-for-[[index]]'
+		};
+
+	let settings,
+		itemEls,
+		itemElClone,
+		idPatternDefault = 'data-ffa-id-pattern',
+		namePatternDefault = 'data-ffa-name-pattern',
+		forPatternDefault = 'data-ffa-for-pattern',
+		defaults = {
+			blueprint: null,
+			itemClass: 'js-ffa-item',
+			addTriggerSelector: '.js-ffa-add-trigger',
+			removeTriggerSelector: '.js-ffa-remove-trigger',
+			idPatternAttr: 'data-ffa-id-pattern',
+			namePatternAttr: 'data-ffa-name-pattern',
+			forPatternAttr: 'data-ffa-for-pattern',
+			disabledClass: 'is-disabled',
+			initialReindex: 1,
+			firstItemIsRemovable: true,
+			onlyLastItemIsRemovable: false,
+			onItemAddCallback: null,
+			onItemRemoveCallback: null,
+			resetNewItemFormVals: true
+		};
+
+	function init() {
+		settings = Object.assign({}, defaults, options);
+
+		if (wrapperEl) {
+			if (settings.blueprint !== null) {
+				buildItemElsFromBlueprint();
+			}
+			itemEls = Array.from(wrapperEl.querySelectorAll('.' + settings.itemClass));
+			if (itemEls.length > 0) {
+				itemElClone = resetNewItemFormVals(itemEls[0].cloneNode(true));
+				disableFirstItemRemoveTrigger();
+				setTriggerDisabledState(itemElClone.querySelector(settings.addTriggerSelector), false);
+				reindexItems();
+				bindEvents();
+			} else {
+				let err = settings.blueprint !== null ? errors.noItemElsFoundInBlueprint : errors.noItemElsFoundInMarkup;
+				logError(err);	
+			}
+		} else {
+			logError(errors.noWrapperEl);	
+		}
+	};
+
+	function buildItemElsFromBlueprint() {
+		emptyElements(wrapperEl);
+		if (isElement(settings.blueprint)) {
+			wrapperEl.appendChild(settings.blueprint);
+		} else {
+			wrapperEl.insertAdjacentHTML('afterbegin', settings.blueprint);
+		}
+		settings.blueprint.classList.add(settings.itemClass);
+	};
+
+	function reindexItems() {
+		for (var itemIndex = settings.initialReindex; itemIndex <= itemEls.length; itemIndex++) {
+			let itemEl = itemEls[itemIndex];
+			if (itemEl) {
+				let attrs = Object.keys(defaultPatterns),
+					matchingEls = Array.from(itemEl.querySelectorAll('[' + attrs.join('], [') + ']'));
+
+				matchingEls.forEach(function(el, elIndex) {
+					attrs.forEach(function(attr) {
+						if (el.hasAttribute(attr)) {
+							let itemAttrVal = el.getAttribute(settings[attr + 'PatternAttr']);
+							if (!itemAttrVal) {
+								itemAttrVal = defaultPatterns[attr];
+							}
+							itemAttrVal = itemAttrVal;
+							el.setAttribute(attr, itemAttrVal.replace('[[index]]', itemIndex));
+						}
+					});
+				});
+			}
+			
+		}
+	};
+
+	function disableFirstItemRemoveTrigger() {
+		if (!settings.firstItemIsRemovable) {
+			let triggerEl = itemEls[0].querySelector(settings.removeTriggerSelector);
+			if (triggerEl) {
+				triggerEl.parentNode.removeChild(triggerEl);
+			}
+		}
+	};
+
+	function bindEvents() {
+		wrapperEl.addEventListener('click', onElementClick);
+	};
+
+	function onElementClick(e) {
+		let clickedEl = e.target;
+		if (clickedEl.closest(settings.addTriggerSelector) !== null) {
+			e.preventDefault();
+			addItem(clickedEl);
+		} else if (clickedEl.closest(settings.removeTriggerSelector) !== null) {
+			e.preventDefault();
+			let itemEl = clickedEl.closest('.' + settings.itemClass);
+			removeItem(itemEl);
+		}
+	};
+
+	function addItem(addTriggerEl) {
+		let triggerEls = [
+				addTriggerEl
+			],
+			newItem = itemElClone.cloneNode(true);
+		if (settings.onlyLastItemIsRemovable === true) {
+			let siblingRemoveTriggerEl = getSiblingTrigger(addTriggerEl, settings.removeTriggerSelector);
+			if (siblingRemoveTriggerEl !== null) {
+				triggerEls.push(siblingRemoveTriggerEl);
+			}
+		}
+		triggerEls.forEach(function(triggerEl) {
+			setTriggerDisabledState(triggerEl);
+		});
+		wrapperEl.appendChild(newItem);
+		itemEls.push(newItem);
+		reindexItems();
+		if (settings.onItemAddCallback !== null) {
+			settings.onItemAddCallback(newItem);
+		}
+	};
+
+	function removeItem(itemEl) {
+		let removeItemIndex = itemEls.indexOf(itemEl);
+		itemEls.splice(removeItemIndex, 1);
+		wrapperEl.removeChild(itemEl);
+		if (itemEls.length > 0) {
+			let lastItemEl = itemEls[itemEls.length - 1],
+			lastItemAddTriggerEl = lastItemEl.querySelector(settings.addTriggerSelector);
+			setTriggerDisabledState(lastItemAddTriggerEl, false);
+		}
+		reindexItems();
+		if (settings.onItemRemoveCallback !== null) {
+			settings.onItemRemoveCallback(itemEl);
+		}
+	};
+
+	function setTriggerDisabledState(triggerEl, isDisabled = true) {
+		let ariaAttr = 'aria-disabled',
+			classListMethodName = isDisabled === true ? 'add' : 'remove';
+		if (triggerEl) {
+			triggerEl.classList[classListMethodName](settings.disabledClass);
+			triggerEl.setAttribute(ariaAttr, isDisabled);
+			triggerEl.disabled = isDisabled;
+		}
+	};
+
+	function resetNewItemFormVals(item) {
+		if (settings.resetNewItemFormVals === true) {
+			let allInputEls = Array.from(item.querySelectorAll('input', 'textarea', 'select'));
+			allInputEls.forEach(function(input) {
+				switch(input.nodeName) {
+				    case 'TEXTAREA':
+				    	input.innerHTML = '';
+				        break;
+				    default:
+				        input.value = '';
+				        input.checked = false;
+				        break;
+				}
+				
+			});
+		}
+		return item;
+	};
+
+	function getSiblingTrigger(triggerEl, siblingSelector) {
+		return triggerEl.closest('.' + settings.itemClass).querySelector(siblingSelector);
+	};
+
+	function getItems() {
+		return itemEls;
+	};
+
+	function logError(msg, logLevel = 'warn') {
+		console[logLevel](errors.prefix + msg);
+	};
+
+	function destroy() {
+		wrapperEl.removeEventListener('click', onElementClick);
+	};
+
+	init();
+
+	return {
+		destroy: destroy,
+		getItems: getItems
+	}
+
+};
+
+export default formFieldAppender;
